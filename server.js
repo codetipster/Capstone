b278cc61f32327767cd8092fc3380686df76beaa
Codetipster <talk2samuelnzekwe@gmail.com>
2023-07-25 10:36:23 +0200

    fix download bugs, refactor server.js into server2.js which is the latest state

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
 const express = require('express');
-var { AnxApi } = require('anx-api');
+const { AnxApi } = require('anx-api');
+const axios = require('axios');
+const fs = require('fs');
+const csv = require('csv-parser');
+const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const username = process.env.XANDR_USERNAME;
 const password = process.env.XANDR_PASSWORD;
+
 
 const reportRequest = {
     report: {
@@ -18,45 +23,142 @@ const reportRequest = {
     }
 };
 
-var anxApi = new AnxApi({
+
+const anxApi = new AnxApi({
     target: 'https://api.appnexus.com',
     rateLimiting: true
 });
 
+const getReport = async (url, token) => {
+  try {
+    return await axios({
+      method: 'get',
+      url: url,
+      responseType: 'stream',
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+  } catch (error) {
+    if (error.response && error.response.status === 401) {
+      const newToken = await anxApi.login(username, password);
+      anxApi.token = newToken;
+      return await getReport(url, newToken);
+    }
+    throw error;
+  }
+};
+
 app.get('/', async (req, res) => {
-    anxApi.login(username, password).then((token) => {
+    try {
+        const token = await anxApi.login(username, password);
         anxApi.token = token;
-        anxApi.post('report', reportRequest)
-        .then(response => {
-            console.log(JSON.stringify(response, null, 2)); // print out the entire response object with pretty formatting
-            const reportId = response.body.response.report_id;
-            const interval = setInterval(() => {
-                anxApi.get(`report?id=${reportId}`)
-                .then(response => {
-                    console.log(JSON.stringify(response, null, 2)); // print out the entire response object with pretty formatting
-                    if (response.body.response.report.execution_status === "ready") {
-                        clearInterval(interval);
+        console.log(`Logged in with token: ${token}`);
+        const reportResponse = await anxApi.post('report', reportRequest);
 
-                        anxApi.get(`report-download?id=${reportId}`)
-                        .then(response => {
-                            console.log(JSON.stringify(response.body, null, 2)); // print out the entire response object with pretty formatting
-                        })
-                        .catch(error => {
-                            console.error(`Error: ${error}`);
-                            console.log(JSON.stringify(error, null, 2));  // Additional logging
-                        });
-                    }
-                })
-                .catch(error => console.error(`Error: ${error}`));
-            }, 5000);
-        })
-        .catch(error => console.error(`Error: ${error}`));
-    })
-    .catch(error => console.error(`Error: ${error}`));
+        console.log("Report request response: ", JSON.stringify(reportResponse, null, 2)); 
+        const reportId = reportResponse.body.response.report_id;
+        console.log(`Report ID: ${reportId}`);
 
-    res.send('Report request sent!');
+        let reportStatus;
+        do {
+            reportStatus = await anxApi.get(`report?id=${reportId}`);
+            console.log("Report status response: ", JSON.stringify(reportStatus, null, 2));
+            await new Promise(resolve => setTimeout(resolve, 5000));
+        } while (reportStatus.body.response.report.execution_status !== "ready");
+
+        console.log('The report is ready. Starting the download.');
+
+        const downloadResponse = await getReport(`https://api.appnexus.com/report-download?id=${reportId}`, anxApi.token);
+        
+        console.log('The report has been downloaded.');
+        const output = fs.createWriteStream(path.resolve(__dirname, 'report.csv'));
+        downloadResponse.data.pipe(output);
+        await new Promise((resolve, reject) => {
+            output.on('finish', resolve);
+            output.on('error', reject);
+        });
+
+        console.log('The report has been saved.');
+        const results = [];
+        fs.createReadStream(path.resolve(__dirname, 'report.csv'))
+            .pipe(csv())
+            .on('data', (data) => results.push(data))
+            .on('end', () => {
+                console.log(results);
+            });
+
+        res.send('Report request sent and processed!');
+    } catch (error) {
+        console.error(`An error occurred: ${error}`);
+        console.log("Error details: ", JSON.stringify(error, null, 2));
+        res.status(500).send('An error occurred while processing the request.');
+    }
 });
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
+});
+
+
+
+
+
+const getAuthToken = async () => {
+    console.log("username: ", username);
+    console.log("password: ", password);
+
+    try {
+      const response = await axios.post('https://api.adnxs.com/auth', {
+        auth: {
+          "username": username,
+          "password": password
+        }
+      });
+      console.log("response.data.token: ", response.data.token)
+      return response.data.token;  // Adjust this according to the API response structure
+    
+    } catch (error) {
+      console.error("Failed to get auth token:", error);
+      return null;
+    }
+  };
+
+
+
+  const initializeXandrApi = async () => {
+    const authToken = await getAuthToken();
+    if (!authToken) {
+      console.error("Failed to initialize Xandr API due to auth failure.");
+      return null;
+    }
+  
+    return axios.create({
+      baseURL: 'https://api.appnexus.com',
+      headers: {
+        'Authorization': 'Bearer ' + authToken
+      }
+    });
+  };
+
+
+  // Usage:
+initializeXandrApi().then((xandrApi) => {
+    console.log("xandrApi: ", xandrApi);
+    // Now you can use xandrApi to make requests
+    // If auth failed, xandrApi will be null
+  });
+
+
+
+
+
+  // Authenticate with your username and password
+anxApi.login(username, password).then(function (token) {
+    console.log('Authenticated successfully. The session token is', token);
+
+    // Now you can make requests, e.g., to get some data:
+    
+}).catch(function (err) {
+    console.log('Error during authentication:', err);
 });
---
+++ server2.js
@@ -0,0 +1,110 @@
+require('dotenv').config();
+const express = require('express');
+//const { AnxApi } = require('anx-api');
+const axios = require('axios');
+const fs = require('fs');
+const csv = require('csv-parser');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3002;
+
+const username = process.env.XANDR_USERNAME;
+const password = process.env.XANDR_PASSWORD;
+
+var AnxApi = require('anx-api').AnxApi;
+
+
+// Create a new instance with api target
+var anxApi = new AnxApi({
+    target: 'https://api.appnexus.com',
+    rateLimiting: true
+});
+
+anxApi.login(username, password).then((token) => {
+    console.log('Authenticated successfully. The session token is', token);
+    // Set the token for future requests
+    anxApi.token = token;
+    console.log('Authenticated successfully. The session token is saved here for the future', token);
+    app.get('/fetch-report', (req, res) => {
+        const reportRequestData = {
+            report: {
+              "report_type": "network_analytics",
+              "columns": [
+                "publisher_id", 
+                "publisher_name", 
+                "size", 
+                "campaign_id", 
+                "campaign_name", 
+                "campaign_code", 
+                "campaign_priority", 
+                "campaign_type", 
+                "creative_id", 
+                "creative_name", 
+                "creative_code", 
+                "size", 
+                "brand_id", 
+                "brand_name", 
+                "billing_period.start_date", 
+                "billing_period.end_date", 
+                "view_rate", 
+                "view_measurement_rate"
+              ],
+              "report_interval": "yesterday",
+            }
+          };
+          
+          
+        
+        
+        
+
+        anxApi.post('report', reportRequestData)
+            .then(reportResponse => {
+                //console.log('This is response data from Xandr:', reportResponse);
+                const reportId = reportResponse.body.response.report_id;
+                console.log('Report ID:', reportId);
+
+                //const reportUrl = `https://api.appnexus.com/report-download?id=${reportId}`;
+                let intervalId = setInterval(() => {
+                    console.log('Checking report status...');
+                    anxApi.get(`report?id=${reportId}`).then(statusResponse => {
+                        if (statusResponse.body.response.execution_status === "ready") {
+                            console.log('Report ready,  downloading...');
+                            clearInterval(intervalId);
+        
+                            // Download report
+                            axios.get(`https://api.appnexus.com/report-download?id=${reportId}`, {
+                                headers: {
+                                    'Authorization': token // Use the token obtained from login
+                                },
+                                responseType: 'stream'
+                            })
+                            .then(response => {
+                                const file = fs.createWriteStream(`./${reportId}.csv`); // Change file path and name as needed
+                                response.data.pipe(file);
+        
+                                file.on('finish', () => {
+                                    file.close();  // close() is async, call callback after close completes.
+                                    console.log('Report downloaded successfully');
+                                    res.send('Report downloaded successfully');
+                                });
+                            })
+                            .catch(err => {
+                                console.error(err);
+                                res.status(500).send('Failed to download report');
+                            });
+                        }
+                    });
+                }, 60000); // Check status every 30 seconds, adjust as needed
+            })
+            .catch(err => {
+                console.error(err);
+                res.status(500).send('Failed to fetch report');
+            });
+    });
+
+    app.listen(3000, () => {
+        console.log('Server running on port 3000');
+    });
+});
